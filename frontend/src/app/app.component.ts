550881041b4afedfaef3a24680ac8ccccb69396b
unknown <maiagiovanav@gmail.com>
2021-10-27 11:28:25 -0300

    Create app.component.ts

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: "app.component.html",
+})
+export class AppComponent {
+  title = 'frontend';
+  desenvolvedor = "Giovana Vieira Maia";
+  ano = "2021";
+}
