c41a8459a568831a8cbda2e16561a1e4a6ebe0e6
unknown <maiagiovanav@gmail.com>
2021-11-07 13:39:26 -0300

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from "./views/home/home.component";
+import { SeriesComponent } from './views/series/series.component';
+import { SeriesCreateComponent } from './views/series/series-create/series-create.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+  },  
+  {
+    path: "series",
+    component: SeriesComponent,
+  },
+  {
+    path: "series/create",
+    component: SeriesCreateComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
