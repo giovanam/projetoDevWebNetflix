1a3d76459c1391c07441d5c976fb20ddaf976d17
unknown <maiagiovanav@gmail.com>
2021-11-10 11:42:53 -0300

    Update series-create.component.ts

--- frontend/src/app/components/series/series-create/series-create.component.ts
+++ frontend/src/app/components/series/series-create/series-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { Series } from '../series.model';
+import { SeriesService } from '../series.service';
 
 @Component({
   selector: 'app-series-create',
@@ -6,14 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./series-create.component.css']
 })
 export class SeriesCreateComponent implements OnInit {
+  series: Series = {
+    nome: "Supergirl",
+    temporadas: "6 temporadas",
+    descricao: "Kara Zor-El escapou de seu planeta quando tinha 12 anos de idade. Desde que chegou à Terra, ela conseguiu esconder seus poderes, os quais compartilha com seu primo, o Super-Homem. Mas agora, ela ajuda a proteger os cidadãos de National City.",
+  }
 
-  constructor(private router: Router) { }
+  constructor(private seriesService: SeriesService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  navigateToProductCreate(): void {
-    this.router.navigate(["/series/create"]);
+  createSeries(): void {
+    this.seriesService.showMessage("Série cadastrada!");
+  }
+
+  cancel(): void {
+    this.router.navigate(["/series"]);
   }
 }
 
