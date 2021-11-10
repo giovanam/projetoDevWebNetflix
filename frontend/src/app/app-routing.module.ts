a13cb1039df16b3044b5c47fa270c65cdd9c22d7
unknown <maiagiovanav@gmail.com>
2021-11-10 11:42:42 -0300

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+//Importação dos comonentes para contéudo
 import { HomeComponent } from "./views/home/home.component";
 import { SeriesComponent } from './views/series/series.component';
-import { SeriesCreateComponent } from './views/series/series-create/series-create.component';
+import { SeriesCreateComponent } from './components/series/series-create/series-create.component';
 
 const routes: Routes = [
   {
