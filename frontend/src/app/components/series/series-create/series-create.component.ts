520d604d830323e09de952def06cf914d26b6d36
unknown <maiagiovanav@gmail.com>
2021-11-16 07:44:50 -0300

    Update series-create.component.ts

--- frontend/src/app/components/series/series-create/series-create.component.ts
+++ frontend/src/app/components/series/series-create/series-create.component.ts
@@ -10,9 +10,9 @@ import { SeriesService } from '../series.service';
 })
 export class SeriesCreateComponent implements OnInit {
   series: Series = {
-    nome: "Supergirl",
-    temporadas: "6 temporadas",
-    descricao: "Kara Zor-El escapou de seu planeta quando tinha 12 anos de idade. Desde que chegou à Terra, ela conseguiu esconder seus poderes, os quais compartilha com seu primo, o Super-Homem. Mas agora, ela ajuda a proteger os cidadãos de National City.",
+    nome: "",
+    temporadas: "",
+    descricao: "",
   }
 
   constructor(private seriesService: SeriesService, private router: Router) { }
